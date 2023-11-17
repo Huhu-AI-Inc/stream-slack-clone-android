afea0e41300474f676e677443bb9b73b6a2b453c
Yixuan Dong <ydong@huhu.ai>
2023-11-16 20:54:36 -0500

    feat: add SessionRepository to call api
    SessionRepository is not yet organized under the same package with other repositories

---
+++ feat-channels/src/main/java/io/getstream/slackclone/uichannels/SessionRepository.kt
@@ -0,0 +1,52 @@
+package io.getstream.slackclone.uichannels
+
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.openapitools.client.apis.SessionApi
+import org.openapitools.client.apis.SessionsApi
+import org.openapitools.client.models.Session
+import org.openapitools.client.models.SessionBody
+
+class SessionRepository {
+
+  suspend fun createSession(sessionBody: SessionBody, accessToken: String): Result<String> =
+    withContext(Dispatchers.IO) {
+      try {
+        val data = SessionApi().createSession(sessionBody, accessToken)
+        Result.success(data)
+      } catch (e: Exception) {
+        Result.failure(e)
+      }
+    }
+
+  suspend fun deleteSession(sessionId: String, accessToken: String): Result<Session> =
+    withContext(Dispatchers.IO) {
+      try {
+        val data = SessionApi().deleteSession(sessionId, accessToken)
+        Result.success(data)
+      } catch (e: Exception) {
+        Result.failure(e)
+      }
+    }
+
+  suspend fun updateSession(sessionId: String, sessionBody: SessionBody, accessToken: String): Result<Session> =
+    withContext(Dispatchers.IO) {
+      try {
+        val data = SessionApi().updateSession(sessionId, sessionBody, accessToken)
+        Result.success(data)
+      } catch (e: Exception) {
+        Result.failure(e)
+      }
+    }
+
+  suspend fun getSessions(startPageId: String? = null, endPageId: String? = null, accessToken: String): Result<Session> =
+    withContext(Dispatchers.IO) {
+      try {
+        val data = SessionsApi().getSessions(startPageId, endPageId, accessToken)
+        Result.success(data)
+      } catch (e: Exception) {
+        Result.failure(e)
+      }
+    }
+}
